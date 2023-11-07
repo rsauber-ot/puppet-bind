2f7c2e0fc2eb9f58d7b75b500467ae9e01baa0df
Robert Sauber <rsauber@opentable.com>
2023-11-07 15:51:49 -0800

    Add support for Ubuntu

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,24 +8,44 @@ class bind::params {
       $servicename       = 'named'
       $binduser          = 'root'
       $bindgroup         = 'named'
+      $file_hint         = 'named.ca'
+      $file_rfc1912      = '/etc/named.rfc1912.zones'
+      if versioncmp($::operatingsystemrelease, '8') >= 0 {
+        $file_bindkeys   = '/etc/named.root.key'
+      } else {
+        $file_bindkeys   = '/etc/named.iscdlv.key'
+      }
     }
     'Debian': {
       $packagenameprefix = 'bind9'
       $servicename       = 'bind9'
       $binduser          = 'bind'
       $bindgroup         = 'bind'
+      $file_rfc1912      = '/etc/bind/named.conf.default-zones'
+      if $::operatingsystem == 'Ubuntu' {
+        $file_hint = false
+      } else {
+        $file_hint = '/etc/bind/db.root'
+      }
+      $file_bindkeys     = '/etc/named.iscdlv.key'
     }
     'Freebsd': {
       $packagenameprefix = 'bind910'
       $servicename       = 'named'
       $binduser          = 'bind'
       $bindgroup         = 'bind'
+      $file_hint         = 'named.ca'
+      $file_rfc1912      = '/etc/named.rfc1912.zones'
+      $file_bindkeys     = '/etc/named.iscdlv.key'
     }
     default: {
       $packagenameprefix = 'bind'
       $servicename       = 'named'
       $binduser          = 'root'
       $bindgroup         = 'named'
+      $file_hint         = 'named.ca'
+      $file_rfc1912      = '/etc/named.rfc1912.zones'
+      $file_bindkeys     = '/etc/named.iscdlv.key'
     }
   }
 
